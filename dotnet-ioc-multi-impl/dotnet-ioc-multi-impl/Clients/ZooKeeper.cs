673891d95ed05b6e87198cb4bb57bf444703f300
GuyKoch <Test1@example.com>
2023-11-24 18:30:17 +0200

    Feature: adding DI using service resolver according to class names at runtime

---
+++ dotnet-ioc-multi-impl/dotnet-ioc-multi-impl/Clients/ZooKeeper.cs
@@ -0,0 +1,22 @@
+﻿using dotnet_ioc_multi_impl.Delegates;
+using dotnet_ioc_multi_impl.Interfaces;
+
+namespace dotnet_ioc_multi_impl.Clients;
+
+public class ZooKeeper
+{
+    private readonly IAnimal? _animal;
+    
+    public ZooKeeper(AnimalResolver animalResolver)
+    {
+        _animal = animalResolver(this.GetType().Name); // Notifies the resolver that it is a zooKeeper
+    }
+
+    /// <summary>
+    /// Feeding the animal
+    /// </summary>
+    public void Work()
+    {
+        _animal?.Feed();
+    }
+}
